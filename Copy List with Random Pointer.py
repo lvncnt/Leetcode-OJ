2d9b6f4b29db259e76749796c007f2a4d5e413bd
feng18 <fengxliu@gmail.com>
2014-12-10 22:07:49 -0500

    Update Copy List with Random Pointer.py

--- Copy List with Random Pointer.py
+++ Copy List with Random Pointer.py
@@ -1,2 +1,33 @@
 ## Copy list with random pointer
-class 
+class RandomListNode():
+  def __init__(self, x): 
+    self.label = x
+    self.next = None 
+    self.random = None 
+    
+def copyRandomList(head): 
+  ## O(n) runtime 
+  p = head 
+  while(p != None): 
+    copy = RandomListNode(p.label)
+    next = p.next 
+    p.next = copy
+    copy.next = next 
+    p = next 
+    
+  p = head
+  while(p != None): 
+    p.next.random = p.random.next if p.random else None 
+    p = p.next.next 
+    
+  p1 = head
+  dummyHead = p.next if p else None  
+  while(p1 != None): 
+    p2 = p1.next 
+    p1.next = p2.next 
+    p1 = p1.next 
+    p2.next = p1.next if p1 else None 
+    
+  return dummyHead 
+    
+  
