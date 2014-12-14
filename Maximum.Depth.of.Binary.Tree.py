3fb64f50d8b5a1aba7de0f5da8d0050d74d62c92
Vincent Lau <fengxliu@gmail.com>
2014-12-14 17:39:11 -0500

    Update Maximum.Depth.of.Binary.Tree.py

--- Maximum.Depth.of.Binary.Tree.py
+++ Maximum.Depth.of.Binary.Tree.py
@@ -1,1 +1,14 @@
 ## Given a binary tree, find its maximum depth.
+# O(n) runtine
+class TreeNode(): 
+  def __init__(self, x): 
+    self.val = x
+    self.left= None
+    self.right = None
+
+def maxDepth(self, root): 
+  if(root == None): return 0
+  leftDepth = self.maxDepth(root.left)
+  rightDepth = self.maxDepth(root.right)
+  return max(leftDepth, rightDepth) + 1
+  
