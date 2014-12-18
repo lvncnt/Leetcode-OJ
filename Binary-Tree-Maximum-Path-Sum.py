da92d50f4b91c5c6922260fb47ff6aaf67cc525a
Vincent Lau <fengxliu@gmail.com>
2014-12-17 23:24:57 -0500

    Update Binary-Tree-Maximum-Path-Sum.py

--- Binary-Tree-Maximum-Path-Sum.py
+++ Binary-Tree-Maximum-Path-Sum.py
@@ -5,6 +5,26 @@
 
 # defing tree node 
 class TreeNode(): 
+  def __init__(self, x): 
+    self.val = x 
+    self.left = None 
+    self.right = None 
+    
+class Solution(): 
+  # Bottom-up approach; O(n) run time 
+  maxSum = -(1<<31) 
+  def maxPathSum(self, root): 
+    self.maxNode(root)
+    return self.maxSum 
+  
+  def maxNode(self, p): 
+    if(p == None): return 0
+    left = self.maxNode(p.left)
+    right = self.maxNode(p.right) 
+    self.maxSum = max(p.val + left + right, self.maxSum) 
+    maxPath = p.val + max(left, right)
+    return maxPath if maxPath > 0 else 0 
+    
   
   
   
