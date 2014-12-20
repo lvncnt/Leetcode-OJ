73a16ef00d7b41ff4cd3d48a385f9b2c4baeae20
Vincent Lau <fengxliu@gmail.com>
2014-12-19 19:15:02 -0500

    Create Single-Number.py

---
+++ Single-Number.py
@@ -0,0 +1,7 @@
+""" 
+Given an array of integer, every elements appears twice except for one. 
+Find that single one. 
+""" 
+
+# using a dict to keep track of the number of times an item appears 
+def 
