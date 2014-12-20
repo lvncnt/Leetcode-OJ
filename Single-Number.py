edda153c79a5669c96825b993f2e4d9dd2967655
Vincent Lau <fengxliu@gmail.com>
2014-12-19 19:21:43 -0500

    Update Single-Number.py

--- Single-Number.py
+++ Single-Number.py
@@ -4,4 +4,9 @@ Find that single one.
 """ 
 
 # using a dict to keep track of the number of times an item appears 
-def 
+def singleNumber(A): 
+  dict = {}
+  for i in range(len(A)): 
+    if(dict.get(A[i]) == None): dict[A[i]] = 1 
+    elif(dict.get(A[i]) == 1): dict.pop(A[i]) 
+  return list(dict.keys())[0] 
