78f482d4ba5408aa79992546565b9348f0aba2fb
Vincent Lau <fengxliu@gmail.com>
2014-12-19 19:44:21 -0500

    Update Single-Number.py

--- Single-Number.py
+++ Single-Number.py
@@ -10,3 +10,15 @@ def singleNumber(A):
     if(dict.get(A[i]) == None): dict[A[i]] = 1 
     elif(dict.get(A[i]) == 1): dict.pop(A[i]) 
   return list(dict.keys())[0] 
+  
+""" 
+Given an array of integer, every elements appears three times except for one, 
+Find that single one. 
+""" 
+def singleNumber(A): 
+  dict = {} 
+  for i in range(len(A)): 
+    dict[A[i]] = dict[A[i]]+1 if dict.get(A[i]) else 1 
+  for key in dict: 
+    if(dict.get(key)): return key 
+    
