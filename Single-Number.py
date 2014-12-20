a7697de42467d1d95b95fe3247947443b4d651ec
Vincent Lau <fengxliu@gmail.com>
2014-12-19 19:50:41 -0500

    Update Single-Number.py

--- Single-Number.py
+++ Single-Number.py
@@ -6,9 +6,9 @@ Find that single one.
 # using a dict to keep track of the number of times an item appears 
 def singleNumber(A): 
   dict = {}
-  for i in range(len(A)): 
-    if(dict.get(A[i]) == None): dict[A[i]] = 1 
-    elif(dict.get(A[i]) == 1): dict.pop(A[i]) 
+  for i in A: 
+    if(dict.get(i) == None): dict[i] = 1 
+    elif(dict.get(i) == 1): dict.pop(i) 
   return list(dict.keys())[0] 
   
 """ 
@@ -17,8 +17,8 @@ Find that single one.
 """ 
 def singleNumber(A): 
   dict = {} 
-  for i in range(len(A)): 
-    dict[A[i]] = dict[A[i]]+1 if dict.get(A[i]) else 1 
+  for i in A: 
+    dict[i] = dict[i]+1 if dict.get(i) else 1 
   for key in dict: 
     if(dict.get(key)): return key 
     
