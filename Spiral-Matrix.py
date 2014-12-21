ebfa8603517ffeade7f665a8b8ca9e689021fed6
Vincent Lau <fengxliu@gmail.com>
2014-12-20 19:42:18 -0500

    Update Spiral-Matrix.py

--- Spiral-Matrix.py
+++ Spiral-Matrix.py
@@ -9,16 +9,25 @@ def spiralMatrix(matrix):
   left, right = 0, n - 1
   dir = 0 
   output = [] 
+  
   while(top <= bottom and left <= right): 
+    
     if(dir == 0): 
       for i in range(left, right + 1): output.append(matrix[top][i]) 
       top += 1
+      
     elif(dir == 1): 
       for i in range(top, bottom + 1): output.append(matrix[i][right]) 
       right -= 1
+      
     elif(dir == 2): 
       for i in reversed(range(left, right + 1)): output.append(matrix[bottom][i]) 
       bottom -= 1
-    elif(dir == 3)
+      
+    elif(dir == 3): 
+      for i in reversed(range(top, bottom + 1)): output.append(matrix[i][left]) 
+      left += 1
+      
     dir = (dir + 1)%4
+    
   return output 
