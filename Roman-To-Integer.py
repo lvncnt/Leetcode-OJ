56359c56c00019c1957d7c54fff8494b4bdd2db1
Vincent Lau <fengxliu@gmail.com>
2014-12-24 18:46:38 -0500

    Update Roman-To-Integer.py

--- Roman-To-Integer.py
+++ Roman-To-Integer.py
@@ -1,5 +1,20 @@
 """
 Given a roman numeral, convert it to an integer. 
 """ 
-
-def romanToInt(s): 
+class Solution(): 
+  dict = {
+    'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+    'C': 100, 'D': 500, 'M': 1000
+    }
+    
+  def romanToInt(self, s): 
+    output = 0 
+    prev = 0 
+    for i in s: 
+      curr = self.dict.get(i) 
+      output += curr
+      if(prev < curr): output -= (2 * prev) 
+      prev = curr 
+  
+    return output 
+    
