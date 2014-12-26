35499888b7613d40a1beac088191f0ac9a5ddb5d
Vincent Lau <fengxliu@gmail.com>
2014-12-25 21:04:55 -0500

    Update Clone-Graph.py

--- Clone-Graph.py
+++ Clone-Graph.py
@@ -4,3 +4,13 @@ Clone an undirected graph. Each node in the graph contains a label and a list of
 
 # definition of a undirected graph node 
  
+class UndirectedGraphNode: 
+ def __init__(self, x): 
+  self.label = x 
+  self.neighbors = [] 
+  
+# Breadth-First
+class Solution(): 
+ def cloneGraph(self, node): 
+  if(node == None): return node
+  
