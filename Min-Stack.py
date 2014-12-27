c0302d2998a55c60d8c09937c64051a8608ce862
vincent f. liu <fengxliu@gmail.com>
2014-12-27 17:27:38 -0500

    Update Min-Stack.py

--- Min-Stack.py
+++ Min-Stack.py
@@ -2,4 +2,27 @@
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time. 
 """ 
 
-class 
+class MinStack():
+  
+  def __init__(self): 
+    self.stack = [] 
+    self.minStack = [] 
+    
+  # Push element x onto stack 
+  def push(self, x): 
+    self.stack.append(x) 
+    if(len(self.minStack) == 0 or self.minStack[-1] >= x): 
+      self.minStack.append(x) 
+    
+  # Remove the element on top of the stack 
+  def pop(self): 
+    if(self.stack.pop() == self.minStack[-1]):
+      self.minStack.pop() 
+      
+  # Get the top element
+  def top(self): 
+    return self.stack[-1]  
+  
+  # Retrieve the minimum element in the stack 
+  def getMin(self): 
+    return self.minStack[-1]  
