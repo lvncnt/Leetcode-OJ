baf447f8505158022a2d6be1240dd56db366faa5
vincent f. liu <fengxliu@gmail.com>
2014-12-29 17:39:01 -0500

    Create Climb-Stairs.py

---
+++ Climb-Stairs.py
@@ -0,0 +1,6 @@
+""" 
+You are climbing a stair case. 
+It takes n steps to reach to the top. 
+Every time you either climb 1 step or 2 steps. 
+In how many distinct ways can you climb to the top? 
+""" 
