a7836890b4a7709a6db176059eaaad6c76fd4955
vincent f. liu <fengxliu@gmail.com>
2015-01-01 00:09:39 -0500

    Update Maximum-Sum-Subarray.py

--- Dynamic-Programming/Maximum-Sum-Subarray.py
+++ Dynamic-Programming/Maximum-Sum-Subarray.py
@@ -4,4 +4,25 @@ Find the contiguous subarray within an array
 which has the largest sum.
 """ 
 
+# Brute force: O(n^2) runtime
+# Time limit exceed 
 class Solution(): 
+  def maxSubArray(self, A): 
+    maxSoFar = min(A) 
+    for i in range(1, len(A) + 1):
+      for j in range(len(A) - i + 1):
+        sumHere = sum(A[j:j+i])
+        maxSoFar = max(maxSoFar, sumHere) 
+    return maxSoFar 
+    
+# Dynamic programming
+# O(n) runtime 
+class Solution(): 
+  def maxSubArray(self): 
+    maxSoFar = A[0] 
+    maxHere = A[0] 
+    for i in A[1:]: 
+      maxHere = max(maxHere + i, i) 
+      maxSoFar = max(maxSoFar, maxHere) 
+    return maxSoFar 
+  
