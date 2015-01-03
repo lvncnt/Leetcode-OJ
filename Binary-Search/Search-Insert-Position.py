b5f91f3b6603f63d7f8e41391f0511c4d9e17913
vincent f. liu <fengxliu@gmail.com>
2015-01-02 20:40:49 -0500

    Update Search-Insert-Position.py

--- Binary-Search/Search-Insert-Position.py
+++ Binary-Search/Search-Insert-Position.py
@@ -4,3 +4,14 @@ return the index if the target is found.
 If not, return the index where it would 
 be if it were inserted in order.
 """ 
+
+class Solution(): 
+  def searchInsert(self, A, target): 
+    start, end = 0, len(A) - 1
+    while(start <= end): 
+      mid = (start + end)//2
+      if(A[mid] < target): start = mid + 1
+      elif(A[mid] > target): end = mid - 1
+      else: return mid 
+    return end + 1 
+    
