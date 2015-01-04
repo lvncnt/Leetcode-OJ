e937575957b7ac7655d99167cf0391073007bfad
vincent f. liu <fengxliu@gmail.com>
2015-01-03 22:28:54 -0500

    Update Search-in-Rotated-Sorted-Array.py

--- Binary-Search/Search-in-Rotated-Sorted-Array.py
+++ Binary-Search/Search-in-Rotated-Sorted-Array.py
@@ -15,6 +15,16 @@ Assume no duplicates in the array
 
 class Solution(): 
     def search(self, A, target): 
-        pass 
+        start, end = 0, len(A); 
+        while(start <= end): 
+          mid = (start + end )//2
+          if(A[mid] == target): return mid 
+          if(A[start] <= A[mid]): 
+            if(target < A[mid] and target >= A[start]): end = mid - 1
+            else: start = mid + 1
+          else: 
+            if(target > A[mid] and target <= A[end]): start = mid + 1
+            else: end = mid - 1
+        return -1
         
     
