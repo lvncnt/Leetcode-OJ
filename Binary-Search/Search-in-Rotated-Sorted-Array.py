b9e20542ed07a10739c2c7621866f08eaf41a968
vincent f. liu <fengxliu@gmail.com>
2015-01-03 22:24:08 -0500

    Create Search-in-Rotated-Sorted-Array.py

---
+++ Binary-Search/Search-in-Rotated-Sorted-Array.py
@@ -0,0 +1,20 @@
+'''
+Created on Jan 3, 2015
+
+Description: 
+Suppose a sorted array is rotated at 
+some pivot unknown to you beforehand. 
+
+You are given a target value to search.
+
+If found in the array return its index, 
+otherwise return -1.  
+  
+Assume no duplicates in the array
+'''
+
+class Solution(): 
+    def search(self, A, target): 
+        pass 
+        
+    
