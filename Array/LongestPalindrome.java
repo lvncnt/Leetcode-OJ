1f9f08835f57cefd5d055642f8d3da0438af3dbf
vincent f. liu <fengxliu@gmail.com>
2015-01-20 22:37:18 -0500

    Create LongestPalindrome.java

---
+++ Array/LongestPalindrome.java
@@ -0,0 +1,7 @@
+/*
+ * Given a string S, find the longest palindromic substring in S. 
+ * Assume that the maximum length of S is 1000, 
+ * and there exists one unique longest palindromic substring.
+ */
+ 
+ 
