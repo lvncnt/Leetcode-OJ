3c38750953fe50adefb3a2ca6e4bb8bb00430f1e
vincent f. liu <fengxliu@gmail.com>
2015-01-27 15:16:17 -0500

    Update PalindromeNumber.java

--- Math/PalindromeNumber.java
+++ Math/PalindromeNumber.java
@@ -1,2 +1,33 @@
-
+/*
+ * Palindrome Number 
+ * Determine whether an integer is a palindrome. 
+ * Do this without extra space.
+ */
+ 
 class PalindromeNumber{
+  
+  public boolean isPalindrome(int x){
+    if(x < 0) return false; 
+    int div = 1; 
+    while(x/div >= 10){
+      div *= 10; 
+    }
+    int first, last; 
+    while(x != 0){
+      first = x/div; 
+      last = x%10; 
+      if(first != last)return false; 
+      x %= div; 
+      x /= 10; 
+      div /= 100; 
+    }
+    return true; 
+  }
+  
+  public static void main(String[] args) {
+		
+		int x = Integer.parseInt(args[0]); 
+		System.out.println(new Solution().isPalindrome(x));
+
+	}
+}
